025169090c29c2cf5b82c633e11cfc4914fdbbee
Jon Cram <jon@webignition.net>
2019-04-01 15:50:10 +0100

    Exclude by host, scheme, url

--- src/IgnoredUrlVerifier.php
+++ src/IgnoredUrlVerifier.php
@@ -3,19 +3,43 @@
 namespace webignition\IgnoredUrlVerifier;
 
 use webignition\Uri\Host;
+use webignition\Uri\Normalizer;
 use webignition\Uri\Uri;
 
 class IgnoredUrlVerifier
 {
-    public function isUrlIgnored(string $url, array $domainsToIgnore): bool
+    const EXCLUSIONS_HOSTS = 'hosts';
+    const EXCLUSIONS_SCHEMES = 'schemes';
+    const EXCLUSIONS_URLS = 'urls';
+
+    public function isUrlIgnored(string $url, array $exclusions): bool
     {
         $uri = new Uri($url);
-        $host = new Host($uri->getHost());
+        $uri = Normalizer::normalize($uri);
 
-        foreach ($domainsToIgnore as $domainToIgnore) {
-            if ($host->isEquivalentTo(new Host($domainToIgnore))) {
+        $scheme = $uri->getScheme();
+        $excludedSchemes = $exclusions[self::EXCLUSIONS_SCHEMES] ?? [];
+        $excludedSchemes = is_array($excludedSchemes) ? $excludedSchemes : [];
+
+        if (in_array($scheme, $excludedSchemes)) {
+            return true;
+        }
+
+        $host = new Host($uri->getHost());
+        $excludedHosts = $exclusions[self::EXCLUSIONS_HOSTS] ?? [];
+        $excludedHosts = is_array($excludedHosts) ? $excludedHosts : [];
+
+        foreach ($excludedHosts as $excludedHost) {
+            if ($host->isEquivalentTo(new Host($excludedHost))) {
                 return true;
             }
+        }
+
+        $excludedUrls = $exclusions[self::EXCLUSIONS_URLS] ?? [];
+        $excludedUrls = is_array($excludedUrls) ? $excludedUrls : [];
+
+        if (in_array((string) $uri, $excludedUrls)) {
+            return true;
         }
 
         return false;
